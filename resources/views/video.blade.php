4e469e0074728de12a0eb14ff80cd9b99ffe7ce7
windisiswandi <windisiswandi89@gmail.com>
2024-05-15 19:29:40 +0800

    initial commit

---
+++ resources/views/video.blade.php
@@ -0,0 +1,9 @@
+@extends('components.layout')
+
+@section('content')
+    <div class="py-3 px-3 md:py-10">
+        <div>
+            <iframe class="w-full h-[300px] lg:h-[500px]" src="https://www.youtube.com/embed/NjOcnUPzmss?si=LGi6lnTsCVxnJ3Tc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+    </div>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./resources/**/*.blade.php"],
+    theme: {
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1440px",
+            "3xl": "1600px",
+        },
+        extend: {},
+        container: {
+            center: true,
+        },
+        fontFamily: {
+            "open-sans": ['"Open Sans"'],
+        },
+    },
+    plugins: [],
+};
