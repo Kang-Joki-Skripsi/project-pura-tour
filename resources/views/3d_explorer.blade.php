b791aebf514c42a79bc178f119f41d897fc3c017
windisiswandi <windisiswandi89@gmail.com>
2024-05-21 13:13:03 +0800

    feat: update ui 3D explorer

--- resources/views/3d_explorer.blade.php
+++ resources/views/3d_explorer.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="flex flex-col lg:flex-row lg:space-x-3">
         <!-- Video player -->
-        <div class="w-full lg:w-2/3" id="videos"></div>
+        <div class="w-full lg:w-2/3 shadow-lg border border-slate-200 rounded-lg overflow-hidden" id="mainVideo"></div>
 
         <!-- Playlist -->
         <div class="w-full lg:w-1/3 lg:pl-4 mt-5 lg:mt-0">
@@ -15,12 +15,11 @@
 
     <script>
       const item_video = document.getElementById('item-video');
-      const images = document.querySelectorAll('.item-3d-explore');
-      const video_utama = document.querySelector('#videos');
+      const video_utama = document.querySelector('#mainVideo');
 
       function renderVideoTemplate(src) {
         return (`
-            <video class="w-full h-56 sm:h-96 lg:h-[500px]" controls>
+            <video class="w-full h-56 sm:h-96 lg:h-[500px]" controls autoplay>
               <source src="${src}" type="video/mp4">
               Your browser does not support the video tag.
             </video>
@@ -29,11 +28,21 @@
 
       function renderItemVideoTemplate(src) {
         return (`
-            <video id="videoThumbnail" class="w-36 h-20 mr-3 rounded-lg cursor-pointer item-3d-explore mb-3 shadow-lg border border-slate-200">
+            <video class="w-36 h-20 mr-3 rounded-lg cursor-pointer item-3d-explore mb-3 shadow-lg border border-slate-200" data-url="${src}">
                 <source src="${src}" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
         `);
+      }
+
+      function renderItemVideos() {
+        const videos = document.querySelectorAll('.item-3d-explore');
+        videos.forEach(video => {
+          video.addEventListener('click', () => {
+            const src = video.getAttribute('data-url');
+            video_utama.innerHTML = renderVideoTemplate(src)
+          });
+        });
       }
 
       window.addEventListener('load', async function() {
@@ -45,18 +54,13 @@
           dataVideos.data.forEach(src => {
             item_video.innerHTML += renderItemVideoTemplate(src); 
           });
+
+          renderItemVideos();
         } catch (error) {
           console.log(error)
         }
 
       })
-
-      // Tambahkan event listener ke setiap elemen <img>
-      images.forEach((el) => {
-        el.addEventListener('click', () => {
-          const src = el.getAttribute('data-url');
-          iframe.setAttribute('src', src);
-        });
-      });
+      
     </script>
 @endsection
