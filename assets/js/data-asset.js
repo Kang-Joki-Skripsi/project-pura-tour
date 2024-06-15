5244e8550128348fda6ad5d4e35d1441ad9c77a6
windisiswandi <windisiswandi89@gmail.com>
2024-06-15 17:10:17 +0800

    add asset tangible pelinggih

--- assets/js/data-asset.js
+++ assets/js/data-asset.js
@@ -44,14 +44,66 @@ const dataFotoAsset = [
     id: 6,
     type: 'bale',
     metadata: 'https://nusaheritage.id/objekmetadata/warisanberwujud/candi-bentar-pertama',
-    img: ['../assets/img/tangible/bale/Bale sekenem/depan.jpg', '../assets/img/tangible/bale/Bale sekenem/samping1.jpg', '../assets/img/tangible/bale/Bale sekenem/samping2.jpg'],
+    img: [
+      '../assets/img/tangible/bale/Bale sekenem/depan.jpg',
+      '../assets/img/tangible/bale/Bale sekenem/samping1.jpg',
+      '../assets/img/tangible/bale/Bale sekenem/samping2.jpg'
+    ],
     deskripsi: 'Bale Sekenem',
   },
   {
     id: 7,
     type: 'bale',
     metadata: 'https://nusaheritage.id/objekmetadata/warisanberwujud/candi-bentar-pertama',
-    img: ['../assets/img/tangible/bale/Bale sekepat/depan.jpg', '../assets/img/tangible/bale/Bale sekepat/samping1.jpg', '../assets/img/tangible/bale/Bale sekepat/samping2.jpg', '../assets/img/tangible/bale/Bale sekepat/samping3.jpg'],
+    img: [
+      '../assets/img/tangible/bale/Bale sekepat/depan.jpg',
+      '../assets/img/tangible/bale/Bale sekepat/samping1.jpg',
+      '../assets/img/tangible/bale/Bale sekepat/samping2.jpg',
+      '../assets/img/tangible/bale/Bale sekepat/samping3.jpg'
+    ],
     deskripsi: 'Bale Sekepat',
+  },
+  {
+    id: 8,
+    type: 'pelinggih',
+    metadata: 'https://nusaheritage.id/objekmetadata/warisanberwujud/candi-bentar-pertama',
+    img: [
+      "../assets/img/tangible/pelinggih/Penyungsungan betara alit sakti dibukit/depan.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara alit sakti dibukit/samping.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara alit sakti dibukit/samping2.jpg",
+    ],
+    deskripsi: 'Penyungsungan Betara Alit Sakti Dibukit',
+  },
+  {
+    id: 9,
+    type: 'pelinggih',
+    metadata: 'https://nusaheritage.id/objekmetadata/warisanberwujud/candi-bentar-pertama',
+    img: [
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung agung/depan.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung agung/samping.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung agung/samping1.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung agung/samping2.jpg",
+    ],
+    deskripsi: 'Penyungsungan Betara Alit Gunung Agung',
+  },
+  {
+    id: 10,
+    type: 'pelinggih',
+    metadata: 'https://nusaheritage.id/objekmetadata/warisanberwujud/candi-bentar-pertama',
+    img: [
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung rinjani/depan.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung rinjani/samping1.jpg",
+      "../assets/img/tangible/pelinggih/Penyungsungan betara gunung rinjani/samping2.jpg",
+    ],
+    deskripsi: 'Penyungsungan Betara Alit Gunung Rinjani',
+  },
+  {
+    id: 11,
+    type: 'pelinggih',
+    metadata: 'https://nusaheritage.id/objekmetadata/warisanberwujud/candi-bentar-pertama',
+    img: [
+      "../assets/img/tangible/pelinggih/Penyungsungan Betara Ngerurah/depan.jpg",
+    ],
+    deskripsi: 'Penyungsungan Betara Ngerurah',
   },
 ];
